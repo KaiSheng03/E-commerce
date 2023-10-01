cf26ca6ae9329359ef9f58f2b5e067723d75ce74
KaiSheng03 <mankaisheng3@gmail.com>
2023-10-01 17:56:24 +0800

    Update route.py

--- ecommerce/market/route.py
+++ ecommerce/market/route.py
@@ -4,6 +4,7 @@ from market.models import Item, User
 from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db
 from flask_login import login_user, logout_user, login_required, current_user
+
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home')
 def home():
